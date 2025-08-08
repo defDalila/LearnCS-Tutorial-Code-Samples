6fc1ec2fa2e5c049385aa85b192d079e9fcf74d9
defdalila <defdalila@outlook.com>
2025-08-08 06:46:21 -0300

    feat(examples): add async sequence example in Exemplo04
    Introduce a new example, Exemplo04, demonstrating asynchronous programming with IAsyncEnumerable and task-based patterns. Update Program.cs to include Exemplo04 in the conditional compilation section.

---
+++ 01-TourCsharp/Exemplos/Exemplo04.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace TourCsharp.Exemplos;
+
+public static class Exemplo04
+{
+    public static void Run()
+    {
+        Console.WriteLine("Foi Triste Esse - no Aplicativo de Console");
+    }
+    public static async IAsyncEnumerable<int> ReadSequence()
+    {
+        int index = 0;
+        while (index < 100)
+        {
+            int[] nextChunk = await GetNextChunk(index);
+            if (nextChunk.Length == 0)
+            {
+                yield break;
+            }
+            foreach (var item in nextChunk)
+            {
+                yield return item;
+            }
+            index++;
+        }
+
+        await foreach (var number in ReadSequence())
+        {
+            Console.WriteLine(number);
+        }
+    }
+
+    private static Task<int[]> GetNextChunk(int index)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
+
+
--- 01-TourCsharp/Program.cs
+++ 01-TourCsharp/Program.cs
@@ -5,7 +5,6 @@ Console.WriteLine("Hellow Povão do WoorldiMeuDeus!!");
 Console.WriteLine();
 
 
-
 #if false
 
 // Expressões de Correspondência
@@ -15,10 +14,12 @@ Exemplo01.Run();
 // Expressões de coleção
 Exemplo02.Run();
 
-#else
 
 // consulta integrada a Linguagem LINQ
 Exemplo03.Run();
+#else
+//  modelo de programação assíncrona baseado em tarefas 
+Exemplo04.Run();
 
 #endif
 
